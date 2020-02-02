3c6057feb18cfd3d878b3fcc3bef40d113108091
sdcodeaesthetic <swarnadippramanik09@gmail.com>
2020-02-02 17:10:39 +0530

    timer functionality added

--- timer.py
+++ timer.py
@@ -1,33 +1,67 @@
 from tkinter import *
 
 
+class Timer_Application(Frame):
+
+    def __init__(self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+        self.master = master
+        self.running = True
+        self.hrs = 0
+        self.mins = 0
+        self.secs = 0
+        self.app_interface()
+
+    def app_interface(self):
+        self.upperframe = Frame(root)
+        self.upperframe.pack()
+        self.bottomframe = Frame(root)
+        self.bottomframe.pack()
+        self.hours = Entry(self.upperframe, width = 5, font = "Helvetica 46 bold", justify = "center", bg = "lavender")
+        self.hours.grid(row = 0, column = 0, ipady = 47)
+        self.colon_button1 = Button(self.upperframe, text = ':', width = 1, height = 4, font = "Helvetica 24", fg = "black", bg = "LightPink1", relief = SUNKEN)
+        self.colon_button1.grid(row = 0, column = 1)
+        self.minutes = Entry(self.upperframe, width = 5, font = "Helvetica 46 bold", justify = "center", bg = "lavender")
+        self.minutes.grid(row = 0, column = 2, ipady = 47)
+        self.colon_button2 = Button(self.upperframe, text = ':', width = 1, height = 4, font = "Helvetica 24", fg = "black", bg = "LightPink1", relief = SUNKEN)
+        self.colon_button2.grid(row = 0, column = 3)
+        self.seconds = Entry(self.upperframe, width = 5, font = "Helvetica 46 bold", justify = "center", bg = "lavender")
+        self.seconds.grid(row = 0, column = 4, ipady = 47)        
+        self.button_start = Button(self.bottomframe, text = 'Start', width = 25, font = "Calibri 33 bold", fg = "black", bg = "orange", command = lambda: self.start_timer())
+        self.button_start.pack(pady = 5)
+        self.button_stop = Button(self.bottomframe, text = 'Stop', width = 25, font = "Calibri 33 bold", fg = "black", bg = "orange", state = 'disabled', command = lambda: self.stop_timer)
+        self.button_stop.pack(pady = 5)
+        self.button_reset = Button(self.bottomframe, text = 'Reset', width = 25, font = "Calibri 33 bold", fg = "black", bg = "orange", state = 'disabled', command = lambda: self.reset_timer)
+        self.button_reset.pack(pady = 5)
+        
+    def calculate(self):
+        if self.running:
+            if self.secs <= 0:
+                self.running = False
+            else:
+                self.seconds.delete(0, END)
+                self.secs -= 1
+                self.seconds.insert(10, self.secs)
+                self.after(1000, self.calculate)
+
+    def start_timer(self):
+        self.hrs = int(self.hours.get())
+        self.mins = int(self.minutes.get())
+        self.secs = int(self.seconds.get())
+        h = self.hrs <= 23 and self.hrs >= 0
+        m = self.mins <= 59 and self.mins >= 0
+        s = self.secs <= 59 and self.mins >= 0
+        if h and m and s:
+            self.calculate()
+
+    def stop_timer(self):
+        return
+
+    def reset_timer(self):
+        return
+
 if __name__ == "__main__":
     root = Tk()
     root.title("Timer")
-    
-    upperframe = Frame(root, bg = "azure")
-    upperframe.pack()
-    var1 = IntVar()
-    var2 = IntVar()
-    var3 = IntVar()
-    hours = Entry(upperframe, textvariable = var1, width = 5, font = "Helvetica 46 bold", justify = "center", bg = "lavender")
-    hours.grid(row = 0, column = 0, ipady = 47)
-    colon_button1 = Button(upperframe, text = ':', width = 1, height = 4, font = "Helvetica 24", fg = "black", bg = "LightPink1", relief = SUNKEN)
-    colon_button1.grid(row = 0, column = 1)
-    minutes = Entry(upperframe, textvariable = var2, width = 5, font = "Helvetica 46 bold", justify = "center", bg = "lavender")
-    minutes.grid(row = 0, column = 2, ipady = 47)
-    colon_button2 = Button(upperframe, text = ':', width = 1, height = 4, font = "Helvetica 24", fg = "black", bg = "LightPink1", relief = SUNKEN)
-    colon_button2.grid(row = 0, column = 3)
-    seconds = Entry(upperframe, textvariable = var3, width = 5, font = "Helvetica 46 bold", justify = "center", bg = "lavender")
-    seconds.grid(row = 0, column = 4, ipady = 47)
-
-    bottomframe = Frame(root, bg = "azure")
-    bottomframe.pack()
-    button_start = Button(bottomframe, text = 'Start', width = 25, font = "Calibri 33 bold", fg = "black", bg = "orange")
-    button_start.pack(pady = 5)
-    button_stop = Button(bottomframe, text = 'Stop', width = 25, font = "Calibri 33 bold", fg = "black", bg = "orange", state = 'disabled')
-    button_stop.pack(pady = 5)
-    button_reset = Button(bottomframe, text = 'Reset', width = 25, font = "Calibri 33 bold", fg = "black", bg = "orange", state = 'disabled')
-    button_reset.pack(pady = 5)
-
+    Timer_Application(root)
     root.mainloop()
